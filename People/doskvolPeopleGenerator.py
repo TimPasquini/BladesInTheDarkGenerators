22a7fbec1c356ea8514e21ed5242f8867aeb9da5
Tim Pasquini <tim.pasquini@gmail.com>
2021-07-24 14:52:02 -0400

    Finished people generator

--- People/doskvolPeopleGenerator.py
+++ People/doskvolPeopleGenerator.py
@@ -17,6 +17,7 @@ def json_retreiver(json_filename):
 
 heritage = json_retreiver("heritage.json")
 gender = json_retreiver("gender.json")
+appearance = json_retreiver("appearance.json")
 goals = json_retreiver("goals.json")
 methods = json_retreiver("methods.json")
 common_profession = json_retreiver("common_profession.json")
@@ -34,11 +35,21 @@ chosen_person = sys.argv[1]
 
 if chosen_person == "rare":
     print(
-        f"{random.choice(first_name)} '{random.choice(aliases)}' {random.choice(family_name)} is a {random.choices(heritage, weights=[50, 10, 5, 5, 5, 5])}"
+        f"""
+        {random.choice(first_name)} '{random.choice(aliases)}' {random.choice(family_name)}.
+        A/An {random.choice(appearance)} {random.choice(gender)} {random.choices(heritage, weights=[50, 10, 5, 5, 5, 5])[0]} wearing/using a/an {random.choice(style)}.
+        They work as a {random.choice(rare_profession)} and use {random.choice(methods)} to try and gain/cause {random.choice(goals)}.
+        Overall, they seem {random.choice(traits)} but are also {random.choice(quirks)} They are interested in {random.choice(interests)}.
+        """
     )
 elif chosen_person == "common":
     print(
-        f""
+        f"""
+        {random.choice(first_name)} '{random.choice(aliases)}' {random.choice(family_name)}.
+        A {random.choice(appearance)} {random.choice(gender)} {random.choices(heritage, weights=[50, 10, 5, 5, 5, 5])[0]} wearing/using a/an {random.choice(style)}.
+        They work as a {random.choice(common_profession)} and use {random.choice(methods)} to try and gain/cause {random.choice(goals)}.
+        Overall, they seem {random.choice(traits)} but are also {random.choice(quirks)} They are interested in {random.choice(interests)}.
+        """
     )
 else:
     print("Please enter 'rare' or 'common' as an argument")
